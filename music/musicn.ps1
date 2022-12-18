f437ab6d5ae9a7bb67099b489f2257a315fa75a8
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2022-12-18 17:26:33 +0700

    22107.200_update2 - Quick fix commit
    Fixed an issue where it won't download the 4th part of the music archive (Well it was not supposed to be there, but the update to the music archives born the 4th file).

--- music/musicn.ps1
+++ music/musicn.ps1
@@ -13,8 +13,16 @@ Import-Module BitsTransfer
 $m1 = "https://github.com/Bionic-OSE/BioniDKU-music/raw/music/normal.7z.001"
 $m2 = "https://github.com/Bionic-OSE/BioniDKU-music/raw/music/normal.7z.002"
 $m3 = "https://github.com/Bionic-OSE/BioniDKU-music/raw/music/normal.7z.003"
+$m4 = "https://github.com/Bionic-OSE/BioniDKU-music/raw/music/normal.7z.004"
+$m5 = ""
+$m6 = ""
+$m7 = ""
 Start-BitsTransfer -Source $m1 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
 Start-BitsTransfer -Source $m2 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
 Start-BitsTransfer -Source $m3 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
+Start-BitsTransfer -Source $m4 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
+#Start-BitsTransfer -Source $m5 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
+#Start-BitsTransfer -Source $m6 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
+#Start-BitsTransfer -Source $m7 -Destination $PSScriptRoot -RetryInterval 60 -RetryTimeout 70
 
 Start-Process $PSScriptRoot\..\core\7za.exe -Wait -NoNewWindow -ArgumentList "e $PSScriptRoot\normal.7z.001 -o$PSScriptRoot\normal"
