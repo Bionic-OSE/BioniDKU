11e8166237f7f6735163c8b87bbbf430d39e0a59
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2022-12-20 21:26:57 +0700

    22107.200_update3 - Final commit
    Only the version number was bumped. This was an executable repackaging release.

--- core/main7.ps1
+++ core/main7.ps1
@@ -1,5 +1,5 @@
 ﻿$host.UI.RawUI.WindowTitle = "Project BioniDKU - (c) Bionic Butter and Sunryze"
-$butter = "Build 22107.200_pwsh7u2.oseprod_mainrel.221218-1658"
+$butter = "Build 22107.200_pwsh7u3.oseprod_mainrel.221220-2058"
 function Show-Branding($s1,$s2) {
 	if ($s1 -like "clear") {Clear-Host}
 	Write-Host 'Project BioniDKU - Next Generation AutoIDKU' -ForegroundColor White -BackgroundColor Magenta -n; Write-Host ([char]0xA0)
