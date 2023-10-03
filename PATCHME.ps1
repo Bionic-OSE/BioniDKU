255d428ad55eb41e661f32cf0fd7b0c4ae298ce1
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2023-10-04 01:04:12 +0700

    PATCHME.ps1 - WU mode support for 1507 and 1511
    FINALLY after years.

--- PATCHME.ps1
+++ PATCHME.ps1
@@ -1,7 +1,6 @@
 # BioniDKU Download mode & Windows Update mode - Software versions information file
 
 # The purpose of this file is so I can update the version number of apps and the UBRs every month without having to update the whole script package. The UBRs section takes effect starting with version 300_update1, and the software version section starting with version 300_update5.
-# Windows Update mode supports Windows 10, version 1607 and later, which includes PowerShell 5.1. Version 1507 and 1511 are not supported due to 5.0 not having required functions for PSWindowsUpdate to work.
 
 
 # SECTION 1: Software version numbers:
@@ -16,10 +15,12 @@ switch ($edition) {
 	# Consumer and any other editions
 	default {$latest = @(
 		<# ======= EOL builds ======= #>
+		<# 1507 #>          "10240.17446"
+		<# 1511 #>          "10586.1177"
 		<# 1607 #>          "14393.2214"
 		<# 1703 #>          "15063.1418"
 		<# 1709 #>          "16299.1087"
-		<# 1803 #>          "17134.1304"
+		<# 1803 #>          "17134.1304" # .1130?
 		<# 1809 #>          "17763.1577"
 		<# 1903 #>          "18362.1256"
 		<# 1909 #>          "18363.1556"
@@ -32,8 +33,10 @@ switch ($edition) {
 	)}
 	
 	# Commerical editions
-	{$_ -like "Enterprise" -or $_ -like "Education"} {$latest = @(
+	{$_ -like "Enterprise*" -or $_ -like "Education"} {$latest = @(
 		<# ======= EOL builds ======= #>
+		<# 1511 #>          "10586.17446"
+		<# 1511 #>          "10586.1540"
 		<# 1607 #>          "14393.2906"
 		<# 1703 #>          "15063.2108"
 		<# 1709 #>          "16299.2166"
@@ -52,13 +55,14 @@ switch ($edition) {
 	# Long-term servicing editions
 	{$_ -like "EnterpriseS"} {$latest = @(
 		<# ====== Alive builds ====== #>
+		<# LTSB 2015 #>     "10240.20162"
 		<# LTSB 2016 #>     "14393.6252"
 		<# LTSC 2019 #>     "17763.4851"
-		<# LTSC 2021 #>     "19044.3324"
+		<# LTSC 2021 #>     "19044.3448"
 	)}
 	
 	# Server editions (Why? Perhaps Nana can answer that question...)
-	{$_ -like "ServerStandard" -or $_ -like "ServerDatacenter" -or $_ -like "ServerStandardEval" -or $_ -like "ServerDatacenterEval"} {$latest = @(
+	{$_ -like "Server*"} {$latest = @(
 		<# ====== Alive builds ====== #>
 		<# Server 2016 #>   "14393.6252"
 		<# Server 2019 #>   "17763.4851"
