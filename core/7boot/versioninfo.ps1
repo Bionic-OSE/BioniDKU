9d135071cd2544e60af1431d01b59fe0493ace0b
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2023-10-31 01:31:12 +0700

    22107.400_beta4b
    Forgot the version file, OOPS.

--- core/7boot/versioninfo.ps1
+++ core/7boot/versioninfo.ps1
@@ -1,6 +1,6 @@
 # BioniDKU version information file - (c) Bionic Butter
 
 $releasetype = "Beta Release"
-$releaseid = "22107.400_beta4"
-$releaseidex = "22107.400_beta4.osesoft_betarel.231030-0129"
+$releaseid = "22107.400_beta4b"
+$releaseidex = "22107.400_beta4b.osesoft_betarel.231031-0130"
 $utag = "400_b3a"
